845bb76da324500e5fd4a0cc3f6d9df1792dbd80
Chandra <chandra.kethireddy@ucf.edu>
2024-12-18 15:21:54 -0500

    fix go test

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		Terraform: terraform.ProviderFunc(Provider),
+		ProviderFunc: Provider,
 	})
 }
 
 func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"ecr_repository": resourceECRRepository(),
+		},
+	}
+}
+
+func resourceECRRepository() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceECRRepositoryCreate,
+		Read:   resourceECRRepositoryRead,
+		Update: resourceECRRepositoryUpdate,
+		Delete: resourceECRRepositoryDelete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceECRRepositoryCreate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceECRRepositoryRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceECRRepositoryUpdate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceECRRepositoryDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
